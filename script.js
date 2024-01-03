aff75be5d427c056816ce343557f7ae716511395
Andrei-Cristian S <andrei.sandor@outlook.com>
2024-01-03 16:31:56 +0200

    Update animations & styles

--- script.js
+++ script.js
@@ -49,38 +49,83 @@ function attachEventListeners() {
 
 function moveSearchToTop() {
   const container = document.getElementById("container");
-  container.classList.add("top-position");
+    container.classList.add("top-position");
 
-  const searchBar = document.getElementById("ingredientInput");
-  searchBar.classList.add("narrow-bar");
+    const searchBar = document.getElementById("ingredientInput");
+    searchBar.classList.add("narrow-bar");
+
+    const logo = document.getElementById("logo");
+    logo.style.display = "none";
 }
 
 function searchMeals() {
-  moveSearchToTop();
+  showLoading();
+
   const ingredient = document.getElementById("ingredientInput").value;
   fetchMealsByIngredient(ingredient)
-    .then((data) => {
-      if (data.meals) {
-        data.meals.forEach((meal) => {
-          fetchMealDetails(meal.idMeal).then((detailData) => {
-            if (detailData.meals) {
-              displayMeal(detailData.meals[0]);
-            }
-          });
-        });
-      } else {
-        // Handle case when no meals are found
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-      // Handle error in UI
-    });
+      .then((data) => {
+          if (data.meals) {
+              setTimeout(() => {
+                  hideLoading();
+                  document.getElementById("results").innerHTML = "";
+                  moveSearchToTop();
+                  setTimeout(() => {
+                    data.meals.forEach((meal) => {
+                        fetchMealDetails(meal.idMeal).then((detailData) => {
+                            if (detailData.meals) {
+                                displayMeal(detailData.meals[0]);
+                            }
+                        });
+                    });
+                  }, 400);
+                  document.getElementById("results").style.display = "grid";
+              }, 2000);
+          } else {
+            setTimeout(() => {
+                hideLoading();
+                document.getElementById("logo").style.display = "block";
+                document.getElementById("ingredientInput").value = "";
+            }, 2000);
+        }
+      })
+      .catch((error) => {
+          console.error(error);
+          setTimeout(() => {
+            hideLoading();
+            document.getElementById("logo").style.display = "block";
+            document.getElementById("ingredientInput").value = "";
+        }, 2000);
+      });
+}
+
+function showLoading() {
+  document.getElementById("logo").style.display = "none";
+  
+  document.getElementById("ingredientInput").style.display = "none";
+  document.getElementById("searchButton").style.display = "none";
+
+  document.getElementById("loading").style.display = "block";
+}
+
+function hideLoading() {
+  document.getElementById("loading").style.display = "none";
+
+  document.getElementById("ingredientInput").style.display = "block";
+  document.getElementById("searchButton").style.display = "block";
 }
 
 function displayMeal(meal) {
   const resultsDiv = document.getElementById("results");
-  resultsDiv.innerHTML += `<div><h3>${meal.strMeal}</h3><img src="${meal.strMealThumb}" alt="Meal Image"><p>Category: ${meal.strCategory}</p></div>`;
+
+  const mealCard = `
+      <div class="meal-card">
+          <img src="${meal.strMealThumb}" alt="Meal Image" class="meal-image">
+          <div class="card-body">
+              <h5 class="card-title">${meal.strMeal}</h5>
+              <p class="card-text">${meal.strCategory} Category</p>
+          </div>
+      </div>`;
+  resultsDiv.innerHTML += mealCard;
 }
 
 document.addEventListener("DOMContentLoaded", attachEventListeners);
