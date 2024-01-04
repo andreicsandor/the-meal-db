7543347844d0100fee952f0506f019338f034e68
Andrei-Cristian S <andrei.sandor@outlook.com>
2024-01-04 07:52:21 +0200

    Update animations & styles

--- script.js
+++ script.js
@@ -1,122 +1,193 @@
 const API_BASE_URL = "https://www.themealdb.com/api/json/v1/1";
 
-function fetchMealsByIngredient(ingredient) {
-  return fetch(`${API_BASE_URL}/filter.php?i=${ingredient}`, {
-    method: "GET",
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Failed to fetch meals.");
-      }
-      return response.json();
+function getMealsByIngredient(ingredient) {
+  return (
+    fetch(`${API_BASE_URL}/filter.php?i=${ingredient}`, {
+      method: "GET",
     })
-    .then((data) => {
-      if (!data.meals) {
-        throw new Error("No meals found.");
-      }
-      return data;
-    })
-    .catch((error) => {
-      throw error;
-    });
+      .then((response) => {
+        // Throw error if receiving bad response from server
+        if (!response.ok) {
+          throw new Error("Failed to fetch meals.");
+        }
+        // Return server response as JSON
+        return response.json();
+      })
+      .then((data) => {
+        // Throw error if response does not contain data
+        if (!data.meals) {
+          throw new Error("No meals found.");
+        }
+        return data;
+      })
+      // Catch error
+      .catch((error) => {
+        throw error;
+      })
+  );
 }
 
-function fetchMealDetails(mealId) {
-  return fetch(`${API_BASE_URL}/lookup.php?i=${mealId}`, {
-    method: "GET",
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Failed to fetch meal details.");
-      }
-      return response.json();
+function getMealDetails(mealId) {
+  return (
+    fetch(`${API_BASE_URL}/lookup.php?i=${mealId}`, {
+      method: "GET",
     })
-    .then((data) => {
-      if (!data.meals) {
-        throw new Error("No meals found.");
-      }
-      return data;
-    })
-    .catch((error) => {
-      throw error;
-    });
+      .then((response) => {
+        // Throw error if receiving bad response from server
+        if (!response.ok) {
+          throw new Error("Failed to fetch meal details.");
+        }
+        // Return server response as JSON
+        return response.json();
+      })
+      .then((data) => {
+        // Throw error if response does not contain data
+        if (!data.meals) {
+          throw new Error("No meals found.");
+        }
+        return data;
+      })
+      // Catch error
+      .catch((error) => {
+        throw error;
+      })
+  );
 }
 
 function attachEventListeners() {
-  const searchButton = document.getElementById("searchButton");
-  searchButton.addEventListener("click", searchMeals);
+  const submitSearchButton = document.getElementById("submitSearchButton");
+  submitSearchButton.addEventListener("click", searchMeals);
 }
 
 function moveSearchToTop() {
-  const container = document.getElementById("container");
-    container.classList.add("top-position");
+  // Move the container with the search bar & button to the top of the page
+  const searchContainer = document.getElementById("searchContainer");
+  searchContainer.classList.add("top-position");
 
-    const searchBar = document.getElementById("ingredientInput");
-    searchBar.classList.add("narrow-bar");
+  // Update the seach bar & button UI
+  const searchBar = document.getElementById("searchInput");
+  searchBar.classList.add("narrow-bar");
 
-    const logo = document.getElementById("logo");
-    logo.style.display = "none";
+  // Remove the logo
+  const logo = document.getElementById("logo");
+  logo.style.display = "none";
 }
 
 function searchMeals() {
-  showLoading();
+  // Check if the search bar & button are at the top of the page
+  const isTopPosition = document
+    .getElementById("searchContainer")
+    .classList.contains("top-position");
 
-  const ingredient = document.getElementById("ingredientInput").value;
-  fetchMealsByIngredient(ingredient)
-      .then((data) => {
-          if (data.meals) {
-              setTimeout(() => {
-                  hideLoading();
-                  document.getElementById("results").innerHTML = "";
-                  moveSearchToTop();
-                  setTimeout(() => {
-                    data.meals.forEach((meal) => {
-                        fetchMealDetails(meal.idMeal).then((detailData) => {
-                            if (detailData.meals) {
-                                displayMeal(detailData.meals[0]);
-                            }
-                        });
-                    });
-                  }, 400);
-                  document.getElementById("results").style.display = "grid";
-              }, 2000);
+  // Select the results div
+  const searchResultsContainer = document.getElementById(
+    "searchResultsContainer"
+  );
+
+  // Select the message divs
+  const initialPageMessage = document.getElementById("initialPageMessage");
+  const resultsPageMessage = document.getElementById("resultsPageMessage");
+
+  // Clear any previous messages
+  clearMessages();
+
+  if (!isTopPosition) {
+    // Show the home loading transition if the search bar & button are not at the top of the page
+    document.getElementById("logo").style.display = "none";
+    document.getElementById("searchInput").style.display = "none";
+    document.getElementById("submitSearchButton").style.display = "none";
+    document.getElementById("initialPageMessage").style.display = "none";
+    document.getElementById("initialPageSpinner").style.display = "block";
+  } else {
+    // Replace the inner HTML of the results div with the spinner animation
+    const resultsDiv = document.getElementById("searchResultsContainer");
+    resultsDiv.innerHTML =
+      '<div class="results-page-spinner"><i class="fa fa-spinner fa-spin"></i></div>';
+    resultsDiv.style.display = "flex";
+    resultsDiv.style.justifyContent = "center";
+    resultsDiv.style.alignItems = "center";
+  }
+
+  const ingredient = document.getElementById("searchInput").value;
+  getMealsByIngredient(ingredient)
+    .then((data) => {
+      // Treat the case when there are meals
+      if (data.meals) {
+        // Remove the loading animation after 2 seconds
+        setTimeout(() => {
+          if (!isTopPosition) {
+            document.getElementById("initialPageSpinner").style.display =
+              "none";
+            document.getElementById("searchInput").style.display = "block";
+            document.getElementById("submitSearchButton").style.display =
+              "block";
+            moveSearchToTop();
           } else {
-            setTimeout(() => {
-                hideLoading();
-                document.getElementById("logo").style.display = "block";
-                document.getElementById("ingredientInput").value = "";
-            }, 2000);
-        }
-      })
-      .catch((error) => {
-          console.error(error);
+            searchResultsContainer.innerHTML = "";
+          }
+
+          // Fetch and add the meal cards after the search bar is moved to the top of the page
+          data.meals.forEach((meal) => {
+            // Fetch the meal category
+            getMealDetails(meal.idMeal).then((detailData) => {
+              if (detailData.meals) {
+                appendMealToResults(detailData.meals[0]);
+              }
+            });
+          });
+
           setTimeout(() => {
-            hideLoading();
-            document.getElementById("logo").style.display = "block";
-            document.getElementById("ingredientInput").value = "";
+            searchResultsContainer.classList.add("animate-results");
+            searchResultsContainer.style.display = "grid";
+            setTimeout(() => {
+              searchResultsContainer.classList.remove("animate-results");
+            }, 550);
+          }, 500);
         }, 2000);
-      });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      // Remove the loading animation after 2 seconds
+      setTimeout(() => {
+        if (!isTopPosition) {
+          document.getElementById("initialPageSpinner").style.display = "none";
+          document.getElementById("searchInput").style.display = "block";
+          document.getElementById("submitSearchButton").style.display = "block";
+          document.getElementById("logo").style.display = "block";
+        }
+        // Reset the search bar input
+        document.getElementById("searchInput").value = "";
+        // Clear the previous results & hide the results div
+        searchResultsContainer.innerHTML = ``;
+        searchResultsContainer.style.display = "none";
+
+        // Display error message
+        if (!isTopPosition) {
+          initialPageMessage.innerHTML = "Upps, no meals found!";
+          initialPageMessage.style.display = "block";
+        } else {
+          resultsPageMessage.innerHTML = "Upps, no meals found!";
+          resultsPageMessage.style.display = "flex";
+        }
+      }, 2000);
+    });
 }
 
-function showLoading() {
-  document.getElementById("logo").style.display = "none";
-  
-  document.getElementById("ingredientInput").style.display = "none";
-  document.getElementById("searchButton").style.display = "none";
-
-  document.getElementById("loading").style.display = "block";
+function clearMessages() {
+  const initialPageMessage = document.getElementById("initialPageMessage");
+  const resultsPageMessage = document.getElementById("resultsPageMessage");
+  initialPageMessage.style.display = "none";
+  resultsPageMessage.style.display = "none";
 }
 
-function hideLoading() {
-  document.getElementById("loading").style.display = "none";
+function appendMealToResults(meal) {
+  // Select the results div
+  const searchResultsContainer = document.getElementById(
+    "searchResultsContainer"
+  );
 
-  document.getElementById("ingredientInput").style.display = "block";
-  document.getElementById("searchButton").style.display = "block";
-}
-
-function displayMeal(meal) {
-  const resultsDiv = document.getElementById("results");
-
+  // Create the meal card
   const mealCard = `
       <div class="meal-card">
           <img src="${meal.strMealThumb}" alt="Meal Image" class="meal-image">
@@ -125,7 +196,10 @@ function displayMeal(meal) {
               <p class="card-text">${meal.strCategory} Category</p>
           </div>
       </div>`;
-  resultsDiv.innerHTML += mealCard;
+
+  // Append the meal card to the results
+  searchResultsContainer.innerHTML += mealCard;
 }
 
+// Attach event listeners when page is loaded
 document.addEventListener("DOMContentLoaded", attachEventListeners);
